7e1a9a0e2b5560abe0c72aae957b47b0eec6fcf0
tmax818 <tylermaxwell661@gmail.com>
2022-12-12 11:08:54 -0800

    add rest

---
+++ src/main/java/tylermaxwell/HelloHumanStsApplication.java
@@ -0,0 +1,13 @@
+package tylermaxwell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloHumanStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHumanStsApplication.class, args);
+	}
+
+}
